b54948163af968097c0069d120338068744d7fae
Matthew Lagace <mlagace@lymbix.com>
2012-02-09 09:35:40 -0400

    added first files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='SimpleBing',
+      version='0.1',
+      description='Lightweight Python Bing API wrapper',
+      author='Matthew Lagace',
+      author_email='matthewlagace@gmail.com',
+      mainterner='Matthew Lagace',
+      author_email='matthewlagace@gmail.com',
+      py_modules=['bing']
+     )
